3564626719cd39bc350bb356fa654d3f851aaa7f
Omais Rao <154328445+OmaisRao@users.noreply.github.com>
2025-01-13 12:22:42 +0500

    addd

--- index.mjs
+++ index.mjs
@@ -5,22 +5,68 @@ import cors from'cors';
 
 const app = express()
 const port =process.env.PORT || 3000
-app.use(cors())
+const products = []
 
-app.get('/', (req, response) => {
-  //  console.log(`Hello World! from ${req.params.id}`);
-    response.send(`Hello World! from Omais`);
-  })
+app.use(cors({origin:["https://waggish-bat.surge.sh","http://localhost:3000"]}))
+app.use(express.json())
 
-app.get('/Products', (req, response) => {
-  response.send({name:'Lemon',price:180,quantity:'1kg'});
+
+// app.get('/', (req, response) => {
+//   //  console.log(`Hello World! from ${req.params.id}`);
+//     response.send(`Hello World! from Omais`);
+//   })
+
+// app.get('/AllProducts', (req, response) => {
+//   response.send(allproducts);
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+  
+app.get('/', (request, response) => {
+  console.log("ye hy request: ", request.ip);
+
+  response.send(`Hello World 2`)
 })
 
-app.get('/AllProducts', (req, response) => {
-  response.send(allproducts);
+app.get('/products', (request, response) => {
+
+  response.send({ message: "All products fetched successfully", status: 200, data: products, })
+})
+
+app.get('/product/:id', (request, response) => {
+  const index = Number(request.params.id) - 1
+
+  const product = products[index]
+
+  if (!product) {
+      response.status(404).send({ data: null, message: 'product not found' })
+      return;
+  }
+
+  response.status(200).json({ data: product, message: "Product found successfully" })
+})
+
+app.post('/product', (request, response) => {
+  console.log("request ki body: ", request.body);
+
+  const product = request.body
+
+  if (!request.body.productName) {
+      response.status(400).send('name bhi tw batao product ka')
+      return;
+  }
+
+  products.push(product)
+
+  response.status(201).send("product added successfully!")
+})
+
+app.use((request, response) => {
+  response.status(404).send("no route found!")
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-  
